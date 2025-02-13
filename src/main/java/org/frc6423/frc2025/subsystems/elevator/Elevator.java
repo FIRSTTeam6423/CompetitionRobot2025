68f029774b7df385317744725038ccd600ee904e
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-02-12 20:07:44 -0800

    Created elevator constants

--- src/main/java/org/frc6423/frc2025/subsystems/elevator/Elevator.java
+++ src/main/java/org/frc6423/frc2025/subsystems/elevator/Elevator.java
@@ -11,6 +11,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.frc6423.frc2025.Robot;
 
 public class Elevator extends SubsystemBase {
+  /** ELEVATOR CONSTANTS */
+  // mech constants
+  public static final double kReduction = 1 / 2;
+  public static final double kSpoolRadiusMeters = 0.878350;
+  public static final double kRangeMeters = 0.0;
+
+  // Non-scoring poses
+  public static final double kChuteIntakingPoseMeters = 0.0;
+  public static final double kLowerAlgaeRemovePoseMeters = 0.0;
+  public static final double kHigherAlgaeRemovePoseMeters = 0.0;
+
+  // Scoring poses
+  public static final double kL1PoseMeters = 0.0;
+  public static final double kL2PoseMeters = 0.0;
+  public static final double kL3PoseMeters = 0.0;
+  public static final double kL4PoseMeters = 0.0;
+
   private final ElevatorIO m_io;
   private final ElevatorIOInputsAutoLogged m_inputs;
 
