2207e1aecc6f15f2efa7b7c79ff1de7d0f352cbb
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-03-12 21:36:07 -0700

    dsajklhdfl;asdf

--- src/main/java/wmironpatriots/subsystems/swerve/Swerve.java
+++ src/main/java/wmironpatriots/subsystems/swerve/Swerve.java
@@ -1,10 +1,13 @@
 package wmironpatriots.subsystems.swerve;
 
 import wmironpatriots.Robot;
+import wmironpatriots.Constants.MATRIXID;
 import wmironpatriots.subsystems.swerve.module.Module;
 import wmironpatriots.subsystems.swerve.module.ModuleIOComp;
+import wmironpatriots.subsystems.swerve.module.Module.ModuleConfig;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
@@ -14,7 +17,6 @@ import wmironpatriots.utils.mechanismUtils.LoggedSubsystem;
 public class Swerve implements LoggedSubsystem {
     // * CONSTANTS
     public static final double MASS_KG = 54.8847;
-
     public static final double MOI = 5.503;
     public static final double SIDE_LENGTH_METERS = 0.7239;
     public static final double BUMPER_WIDTH_METER = 0.0889; // TODO add value
@@ -28,6 +30,9 @@ public class Swerve implements LoggedSubsystem {
             new Translation2d(-0.381, -0.381)
         };
 
+    public static final double MAX_LINEAR_SPEED = 0.0;
+    public static final double MAX_ANGULAR_SPEED = 0.0;
+
     public static final double LINEAR_P = 0.0;
     public static final double LINEAR_I = 0.0;
     public static final double LINEAR_D = 0.0;
@@ -40,22 +45,36 @@ public class Swerve implements LoggedSubsystem {
     public static final double HEADING_I = 0.0;
     public static final double HEADING_D = 0.0;
 
+    public static final ModuleConfig[] MODULE_CONFIGS = 
+    new ModuleConfig[] {
+        new ModuleConfig(MATRIXID.BL_PIVOT, MATRIXID.BL_DRIVE, MATRIXID.BL_CANCODER, 0.0, true),
+        new ModuleConfig(MATRIXID.FL_PIVOT, MATRIXID.FL_DRIVE, MATRIXID.FL_CANCODER, 0.0, true),
+        new ModuleConfig(MATRIXID.FR_PIVOT, MATRIXID.FR_DRIVE, MATRIXID.FR_CANCODER, 0.0, true),
+        new ModuleConfig(MATRIXID.BR_PIVOT, MATRIXID.BR_DRIVE, MATRIXID.BR_CANCODER, 0.0, true)
+    };
+
     private final Module[] modules;
 
     private final SwerveDriveKinematics kinematics;
     private final SwerveDriveOdometry odo;
 
     public Swerve() {
-        modules = new ModuleIOComp[4];
+        modules = new ModuleIOComp[MODULE_CONFIGS.length];
         for (int i = 0; i < modules.length; i++) {
-            modules[i] = new ModuleIOComp(null);
+            modules[i] = new ModuleIOComp(MODULE_CONFIGS[i]);
         }
+
         kinematics = new SwerveDriveKinematics(MODULE_LOCS);
         odo = new SwerveDriveOdometry(kinematics, getHeading(), getSwerveModulePoses());
     }
 
     public Rotation2d getHeading() {
         return new Rotation2d();
+    }
+
+    public void runVelocities(ChassisSpeeds velocities) {
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(velocities);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, );
     }
 
     public SwerveModuleState[] getSwerveModuleStates() {
