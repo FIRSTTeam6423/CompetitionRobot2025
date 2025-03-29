620fe1e6231372a27657b4d90b8509cc0a0e6ad2
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-03-29 00:52:49 +0000

    converted superstructure into a singleton

--- src/main/java/wmironpatriots/subsystems/superstructure/Superstructure.java
+++ src/main/java/wmironpatriots/subsystems/superstructure/Superstructure.java
@@ -1,5 +1,19 @@
 package wmironpatriots.subsystems.superstructure;
 
 public class Superstructure {
-  
+
+  private static Superstructure instance;
+
+  /**
+   * @return superstructure instance (creates instance if null)
+   */
+  public static Superstructure getInstance() {
+    if (instance == null) {
+      instance = new Superstructure();
+    }
+
+    return instance;
+  }
+
+  private Superstructure() {}
 }
