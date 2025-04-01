7728384b74e5aff8b5d36699e5a61e22157291ac
Dasun L. Abeykoon <Dasun20202020@hotmail.com>
2025-03-31 19:52:28 -0700

    modified hardware interfaces
    Signed-off-by: Dasun L. Abeykoon <Dasun20202020@hotmail.com>

---
+++ src/main/java/wmironpatriots/subsystems/swerve/module/ModuleIOSim.java
@@ -0,0 +1,58 @@
+package wmironpatriots.subsystems.swerve.module;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class ModuleIOSim extends Module {
+  private static final DCMotor pivotMotor = DCMotor.getKrakenX60Foc(1);
+  private static final DCMotor driveMotor = DCMotor.getKrakenX60Foc(1);
+
+  private final DCMotorSim pivotSim, driveSim;
+
+  public ModuleIOSim() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'periodic'");
+  }
+
+  @Override
+  protected void setPivotCurrent(double amps) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setPivotCurrent'");
+  }
+
+  @Override
+  protected void setDriveCurrent(double amps, boolean focEnabled) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setDriveCurrent'");
+  }
+
+  @Override
+  protected void setPivotPose(double poseRevs) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setPivotPose'");
+  }
+
+  @Override
+  protected void setDriveVel(double velMPS) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setDriveVel'");
+  }
+
+  @Override
+  public void stopMotors() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'stopMotors'");
+  }
+
+  @Override
+  protected void enableCoastMode(boolean enabled) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'enableCoastMode'");
+  }
+  
+}
