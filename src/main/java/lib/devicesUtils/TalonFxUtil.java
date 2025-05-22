ae9febe02a1ffb00ea02837f380c9e0f65ab4beb
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-05-22 16:40:47 -0700

    FEAT: Create util class for TalonFx motors

---
+++ src/main/java/lib/devicesUtils/TalonFxUtil.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2025 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/FIRSTTeam6423
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package lib.devicesUtils;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+/** Utility class for managing TalonFX motors */
+public class TalonFxUtil {
+  /**
+   * @return a template {@link TalonFXConfiguration}
+   */
+  public static TalonFXConfiguration getDefaultTalonFxCfg() {
+    TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    cfg.Audio.AllowMusicDurDisable = true;
+    cfg.Audio.BeepOnBoot = true;
+    cfg.Audio.BeepOnConfig = true;
+
+    return cfg;
+  }
+}
