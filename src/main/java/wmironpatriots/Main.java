acb00f1ab82a3d92cc9b1e54661c7b2ae50710a6
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-05-22 16:25:15 -0700

    REFACTOR: Added update javadocs to main class

--- src/main/java/wmironpatriots/Main.java
+++ src/main/java/wmironpatriots/Main.java
@@ -8,10 +8,19 @@ package wmironpatriots;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-// ! DO NOT modify this file unless you know what you're doing
+/**
+ * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
+ * you are doing, do not modify this file except to change the parameter class to the startRobot
+ * call.
+ */
 public final class Main {
   private Main() {}
 
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>If you change your main robot class, change the parameter type.
+   */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
