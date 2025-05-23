8ab5cecf88645140c8deff58c82130030933f34d
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-05-23 09:56:46 -0700

    REFACTOR: Command factory classes shouldn't have shortened names

--- src/main/java/wmironpatriots/commands/factories/SwerveCommandFactory.java
+++ src/main/java/wmironpatriots/commands/factories/SwerveCommandFactory.java
@@ -3,14 +3,13 @@ package wmironpatriots.commands.factories;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import lib.swerve.ChassisVelocity;
 import wmironpatriots.subsystems.Swerve.Swerve;
 
-public class SwerveCmmdFactory {
+public class SwerveCommandFactory {
   private final Swerve swerve;
 
-  public SwerveCmmdFactory(Swerve swerve) {
+  public SwerveCommandFactory(Swerve swerve) {
     this.swerve = swerve;
   }
 
@@ -20,16 +19,14 @@ public class SwerveCmmdFactory {
    * @param xSpeedMagnitude Input stream representing desired x speed magnitude
    * @param ySpeedMagnitude Input stream representing desired y speed magnitude
    * @param angularRateMagnitude Input stream representing desiredd angular rate magnitude
-   * @return
+   * @return {@link Command}
    */
-  public Command teleopDriveCmmd(DoubleSupplier xSpeedMagnitude, DoubleSupplier ySpeedMagnitude, DoubleSupplier angularRateMagnitude) {
-    return Commands.run(() -> {
-      swerve.setVelocitySetpoint(
-        ChassisVelocity.fromFieldRelativeSpeeds(
-          xSpeedMagnitude.getAsDouble(), 
-          ySpeedMagnitude.getAsDouble(), 
-          angularRateMagnitude.getAsDouble(), 
-          swerve.getHeadingRotation2d().getRadians()));
-    }, swerve);
+  public Command teleopDrive(DoubleSupplier xSpeedMagnitude, DoubleSupplier ySpeedMagnitude, DoubleSupplier angularRateMagnitude) {
+    return swerve.setChassisVelocity(
+      ChassisVelocity.fromFieldRelativeSpeeds(
+        xSpeedMagnitude.getAsDouble(), 
+        ySpeedMagnitude.getAsDouble(), 
+        angularRateMagnitude.getAsDouble(), 
+        swerve.getHeadingRotation2d().getRadians()));
   }
 }
